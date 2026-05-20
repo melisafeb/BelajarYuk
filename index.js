0bed21989f8e962ec4ea7312f0d4bcabbafec942
melisafebriana538 <melissafebriana538@gmail.com>
2026-05-20 21:38:43 +0700

    perbaikan

--- index.js
+++ index.js
@@ -1144,7 +1144,7 @@ app.delete('/api/admin/materi/:id', verifyToken, isAdmin, async (req, res) => {
 });
 
 // ====================== START SERVER ======================
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 server.listen(PORT, () => {
   console.log(`🚀 Server jalan di port ${PORT}`);
 });
